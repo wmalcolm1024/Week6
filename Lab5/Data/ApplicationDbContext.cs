6562589fafd064bebef7ccd37f43609e7eda0bd9
wmalcolm1024 <160701958+wmalcolm1024@users.noreply.github.com>
2024-02-25 17:49:28 -0500

    Initial commit

---
+++ Lab5/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab5.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Lab5/Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lab5.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+
+        public string? Message { get; set; }
+        public void OnGet()
+        {
+            Message = $"Hello World The Time Is: {DateTime.Now.ToLongTimeString()}";
+        }
+    }
+}
